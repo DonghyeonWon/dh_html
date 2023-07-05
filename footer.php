56dded2203c62a47bd236f6e6d6f1707adf56672
DonghyeonWon <ehdgus3038@gmail.com>
2023-07-06 00:39:30 +0900

    php로

---
+++ footer.php
@@ -0,0 +1,140 @@
+    <div id="footer">
+        <div class="footer-container">
+            <div class="footer-top">
+                <a href="#" id="footer-logo">
+                    <img src="source/img/logo/mr_architecture_logo.png" alt="푸터로고 자리">
+                </a>
+                <div class="f-vertical"></div>
+                <ul class="footer-topMenu">
+                    <li><a href="#" id="f-toHome">홈</a></li>
+                    <li><a href="#" id="f-policy">이용약관</a></li>
+                    <li><a href="#" id="f-toUse">이용안내</a></li>
+                    <li><a href="#" id="f-personal" class="bolder">개인정보처리방침</a></li>
+                </ul>
+            </div>
+            <div class="m-f-subpages">
+                <div class="m-f-wrap">
+                    <ul class="m-f-depth1 introduce">
+                        <li class="m-f-sub-title"><p>기업소개</p></li>
+                        <li class="m-f-sub-page"><a href="#">인사말</a></li>
+                        <li class="m-f-sub-page"><a href="#">비전</a></li>
+                        <li class="m-f-sub-page"><a href="#">오시는 길</a></li>
+                    </ul>
+                    <ul class="m-f-depth1 business">
+                        <li class="m-f-sub-title"><p>사업영역</p></li>
+                        <li class="m-f-sub-page"><a href="#">건축사업</a></li>
+                        <li class="m-f-sub-page"><a href="#">주택사업</a></li>
+                        <li class="m-f-sub-page"><a href="#">토목사업</a></li>
+                        <li class="m-f-sub-page"><a href="#">조경사업</a></li>
+                    </ul>
+                    <ul class="m-f-depth1 recruit">
+                        <li class="m-f-sub-title"><p>인재채용</p></li>
+                        <li class="m-f-sub-page"><a href="#">인재상</a></li>
+                        <li class="m-f-sub-page"><a href="#">인사제도</a></li>
+                        <li class="m-f-sub-page"><a href="#">채용정보</a></li>
+                    </ul>
+                    <ul class="m-f-depth1 business">
+                        <li class="m-f-sub-title"><p>고객센터</p></li>
+                        <li class="m-f-sub-page"><a href="#">새소식</a></li>
+                        <li class="m-f-sub-page"><a href="#">소셜</a></li>
+                        <li class="m-f-sub-page"><a href="#">문의사항</a></li>
+                    </ul>
+                </div>
+                <div class="m-f-subpages-btn">
+                    <div class="m-f-open">
+                        <p>전체메뉴 열기</p>
+                    </div>
+                    <div class="m-f-close hidden">
+                        <p>전체메뉴 닫기</p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-subpages">
+                <ul class="footer-depth 1">
+                    <li class="sub-title"><a href="#">기업소개</a></li>
+                    <li class="sub-normal"><a href="#">인사말</a></li>
+                    <li class="sub-normal"><a href="#">비전</a></li>
+                    <li class="sub-normal"><a href="#">오시는 길</a></li>
+                </ul>
+                <ul class="footer-depth 2">
+                    <li class="sub-title"><a href="#">사업영역</a></li>
+                    <li class="sub-normal"><a href="#">건축사업</a></li>
+                    <li class="sub-normal"><a href="#">주택사업</a></li>
+                    <li class="sub-normal"><a href="#">토목사업</a></li>
+                    <li class="sub-normal"><a href="#">조경사업</a></li>
+                </ul>
+                <!--
+                <ul class="footer-depth 3">
+                    <li class="sub-title"><a href="#">포트폴리오</a></li>
+                    <li class="sub-normal"><a href="#">건축사업</a></li>
+                    <li class="sub-normal"><a href="#">주택사업</a></li>
+                    <li class="sub-normal"><a href="#">토목사업</a></li>
+                    <li class="sub-normal"><a href="#">조경사업</a></li>
+                </ul>
+            -->
+                <ul class="footer-depth 4">
+                    <li class="sub-title"><a href="#">인재채용</a></li>
+                    <li class="sub-normal"><a href="#">인재상</a></li>
+                    <li class="sub-normal"><a href="#">인사제도</a></li>
+                    <li class="sub-normal"><a href="#">채용정보</a></li>
+                </ul>
+                <ul class="footer-depth 5">
+                    <li class="sub-title"><a href="#">고객센터</a></li>
+                    <li class="sub-normal"><a href="#">새소식</a></li>
+                    <li class="sub-normal"><a href="#">소셜</a></li>
+                    <li class="sub-normal"><a href="#">문의사항</a></li>
+                </ul>
+            </div>
+            <div class="f-horizontal"></div>
+            <div class="footer-socials">
+                <ul class="social-lists">
+                    <li class="f-socialItem youtube"><a href="#"></a></li>
+                    <li class="f-socialItem kakao"><a href="#"></a></li>
+                    <li class="f-socialItem instagram"><a href="#"></a></li>
+                    <li class="f-socialItem twitter"><a href="#"></a></li>
+                </ul>
+            </div>
+            <div class="f-horizontal"></div>
+            <div class="footer-siteInfo">
+                <ul class="f-infoLine">
+                    <li>상호명 : (주)미래건설</li>
+                    <li>대표자 : 홍길동</li>
+                    <li>개인정보책임자 : 고길동</li>
+                </ul>
+                <ul class="f-infoLine">
+                    <li>사업자등록번호 : 000-00-00000</li>
+                    <li>통신판매업신고번호 : 제0000-대한민국-0000호</li>
+                </ul>
+                <ul class="f-infoLine">
+                    <li>연락처 : 000-000-0000,000-000-0001</li>
+                    <li>팩스 : 000-000-0000,000-000-0001</li>
+                    <li>이메일 : webuild@mirae.com</li>
+                </ul>
+                <ul class="f-infoLine">
+                    <li>주소 : 대한민국 대한특별시 민국구 독립로 815 만세빌딩 31층</li>
+                </ul>
+                <ul class="f-infoLine copyright">
+                    <li>Copyright © 2023 미래건설. All rights reserved.</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+    <script type="text/javascript" src="js/javascript.js"></script>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('.mainBanner').slick({
+                infinite: true,
+                dots: true,
+            });
+        });
+    </script>
+
+</body>
+
+</html>
